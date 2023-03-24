7ea7fc9c6c316712d360c5b2676beca0239209bb
Will G <wgrout87@gmail.com>
2023-03-23 21:37:27 -0600

    converted get feed functionality to promise

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -14,6 +14,18 @@ export default function Dashboard() {
     const navigate = useNavigate();
     const baseUrl = state.baseUrl;
 
+    let getFeed = (async () => {
+        const resp = await axios({
+            method: 'GET',
+            url: `${baseUrl}/user/follows/manga/feed?limit=100&translatedLanguage%5B%5D=en&contentRating%5B%5D=safe&contentRating%5B%5D=suggestive&includeFutureUpdates=1&order%5BcreatedAt%5D=desc&order%5BupdatedAt%5D=desc&order%5BpublishAt%5D=desc&order%5BreadableAt%5D=desc&order%5Bvolume%5D=desc&order%5Bchapter%5D=desc`,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${state.sessionToken}`
+            },
+        });
+
+        return resp.data.data;
+    });
 
     useEffect(() => {
         const username = state.username ?? window.localStorage.getItem('username');
@@ -67,18 +79,7 @@ export default function Dashboard() {
                 </Card.Body>
             </Card>
             <Button disabled={refreshingSession} onClick={() => {
-                (async () => {
-                    const resp = await axios({
-                        method: 'GET',
-                        url: `${baseUrl}/user/follows/manga/feed?limit=100&translatedLanguage%5B%5D=en&contentRating%5B%5D=safe&contentRating%5B%5D=suggestive&includeFutureUpdates=1&order%5BcreatedAt%5D=desc&order%5BupdatedAt%5D=desc&order%5BpublishAt%5D=desc&order%5BreadableAt%5D=desc&order%5Bvolume%5D=desc&order%5Bchapter%5D=desc`,
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'Authorization': `Bearer ${state.sessionToken}`
-                        },
-                    });
-
-                    console.log(resp.data);
-                })()
+                getFeed().then(resp => console.log(resp[0]));
             }}>
                 {refreshingSession ? <span className="spinner-border spinner-border-sm"></span> : <span>Get Feed</span>}
             </Button>
