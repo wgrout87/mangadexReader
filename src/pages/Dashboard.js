03a9f74a00a962230adbaf84677b22085e145ab9
Will G <wgrout87@gmail.com>
2023-05-01 20:18:35 -0600

    chapter id accesible on chapter page

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -25,7 +25,7 @@ export default function Dashboard() {
     let refreshToken = state.refreshToken ?? window.localStorage.getItem('refreshToken');
 
     let getFeed = (async () => {
-        // console.log(state.sessionToken);
+        console.log("Session Refresh Status: ", sessionRefreshed);
         const resp = await axios({
             method: 'GET',
             url: `${baseUrl}/user/follows/manga/feed?limit=20&translatedLanguage%5B%5D=en&contentRating%5B%5D=safe&contentRating%5B%5D=suggestive&includeFutureUpdates=1&order%5BcreatedAt%5D=desc&order%5BupdatedAt%5D=desc&order%5BpublishAt%5D=desc&order%5BreadableAt%5D=desc&order%5Bvolume%5D=desc&order%5Bchapter%5D=desc&`,
--- src/pages/IndividualChapter/index.js
+++ src/pages/IndividualChapter/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 export default function IndividualChapter() {
+
+    const chapterId = (window.location.search.replace(/^./, ""));
+    console.log(chapterId);
+
     return (
         <div>
             Manga page
